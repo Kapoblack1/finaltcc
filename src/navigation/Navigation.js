e50f6f611f9b8a91c34a959e7169189e47bd9068
Kapoblack1 <Alepy1@hotmail.com>
2024-04-27 03:21:25 +0100

    second

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -11,6 +11,9 @@ import DriverScreen from '../screens/DriverScreen';
 import PassengerScreen from '../screens/PassengerScreen';
 import TripDetailsScreen from '../screens/TripDetailsScreen';
 import SearchTripsScreen from '../screens/SearchTripScreen';
+import HomeScreenP from '../screens/passenger/HomeScreenP';
+import TravelInformation from '../screens/passenger/TravelInfomation';
+
 
 
 
@@ -20,7 +23,7 @@ const Stack = createStackNavigator();
 export default function Navigation(){
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUpScreen" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="TravelInformation" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen}/>
         <Stack.Screen name="Login1" component={LoginScreen1}/>
         <Stack.Screen name="PhoneVerificationScreen" component={PhoneVerificationScreen}/>
@@ -31,6 +34,8 @@ export default function Navigation(){
         
         <Stack.Screen name="SearchTripsScreen" component={SearchTripsScreen}/>
         <Stack.Screen name="TripDetailsScreen" component={TripDetailsScreen}/>
+        <Stack.Screen name="HomeScreenP" component={HomeScreenP}/>
+        <Stack.Screen name="TravelInformation" component={TravelInformation}/>
 
 
       </Stack.Navigator>
